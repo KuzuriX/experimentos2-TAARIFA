e42e03cddde6e9365083f1501893589208a5f4aa
KuzuriX <ramonosx@Migues-MacBook-Pro.local>
2016-11-30 07:30:40 -0600

    Selección de variables

--- analisis.R
+++ analisis.R
@@ -5,7 +5,7 @@ setwd('/Users/ramonosx/Documents/GitHub/experimentos2-TAARIFA')
 
 # Librerias
 
-
+library('dplyr')
 
 # Carga de los datos
 ## LA VARIABLE A PREDECIR ES status_group
@@ -30,6 +30,8 @@ levels(subvillage)
 
 levels(as.factor(region_code))
 
+length(levels(as.factor(region_code)))
+
 length(levels(as.factor(district_code)))
 
 length(base$region[region_code=99])
@@ -46,3 +48,68 @@ table(region, district_code)
 
 #hola amiguis
 
+table(status_group)
+
+base<- base[!(status_group=="functional needs repair"),] 
+base$status = 1*(base$status_group=='functional') 
+attach(base)
+summary(status)
+
+###### DESCRICION DE LAS VARIABLES 
+# amount_tsh - Total static head (amount water available to waterpoint)
+# funder - Who funded the well
+# gps_height - Altitude of the well
+# installer - Organization that installed the well
+# region_code - Geographic location (coded)
+# subvillage - Geographic location
+# basin - Geographic water basin
+# population - Population around the well
+# scheme_management - Who operates the waterpoint
+# scheme_name - Who operates the waterpoint
+# construction_year - Year the waterpoint was constructed
+# management - How the waterpoint is managed
+# payment_type - What the water costs
+# quality_group - The quality of the water
+# quantity - The quantity of water
+# source - The source of the water
+# waterpoint_type_group - The kind of waterpoint
+
+
+
+##### VARIABLES IMPORTANTES
+#region_code+subvillage+amount_tsh+gps_height+installer+funder
+#+basin+population+scheme_management+scheme_name+construction_year
+#+extraction_type_class+extraction_type_group+management+payment_type
+#+quality_group+quantity+source+waterpoint_type_group
+
+##Variables anidadas 
+#  region_code con subvillage
+# scheme_management con scheme_name
+# extraction_type_class con extraction_type_group
+
+
+#### PRIMER MODELO!!!! WOOOOOOOO
+set.seed(123)
+base1<- base  %>% sample_n(1000)
+
+# 
+# ## 50% of the sample size
+# smp_size <- floor(0.8 * nrow(base))
+# 
+# ## set the seed to make your partition reproductible
+# 
+# train_ind <- sample(seq_len(nrow(train)), size = smp_size)
+# 
+# train1 <- train[train_ind, ]
+# test1 <- train[-train_ind, ]
+
+mod1<-glm(status~region_code+subvillage+amount_tsh+gps_height+installer+funder
+          +basin+population+scheme_management+scheme_name+construction_year
+          +extraction_type_class+extraction_type_group+management+payment_type
+          +quality_group+quantity+source+waterpoint_type_group,
+          family = 'binomial', data = base1)
+
+summary(mod1)
+drop1mod1<-drop1(mod1, test='LRT')
+
+step(lm(mpg~wt+drat+disp+qsec,data=mtcars),direction="backward")
