058e76ec29017251b3f6e1d9b36f7051b9d9e1f6
nataliadiazramirez <natalia.d1411@gmail.com>
2016-12-01 01:56:02 -0600

    comentarios de variables

--- analisis.R
+++ analisis.R
@@ -60,21 +60,43 @@ summary(status)
 
 ###### DESCRICION DE LAS VARIABLES 
 # amount_tsh - Total static head (amount water available to waterpoint)
-# funder - Who funded the well
-# gps_height - Altitude of the well
-# installer - Organization that installed the well
+### funder - Who funded the well  
+                 #---(ESTA SE PODRIA QUITAR SON MUCHAS CATEGORIAS Y HAY BLANCOS)
+# gps_height - Altitude of the well 
+                 #---(HAY NUMEROS NEGATIVOS, CEROS Y POSITIVOS)
+### installer - Organization that installed the well 
+                 #---(ESTA SE PODRIA QUITAR SON MUCHAS CATEGORIAS Y HAY BLANCOS)
 # region_code - Geographic location (coded)
-# subvillage - Geographic location
+                 #----21 REGIONES --nombres en variable region
+########subvillage - Geographic location
+                 #--- HAY MISMOS NOMBRES SOLO QUE UNOS CON A O B HAY MAS DE 9MIL y HAY BLANCOS
 # basin - Geographic water basin
+                 #---9 CUENCAS
 # population - Population around the well
-# scheme_management - Who operates the waterpoint
-# scheme_name - Who operates the waterpoint
+                 #----HAY UNOS CON CERO Y UNOS NUMEROS BAJOS
+##### scheme_management - Who operates the waterpoint
+                 #---BLANCOS 3654- none hay 1
+#####scheme_name - Who operates the waterpoint
+                #---BLANCOS 26162- MUCHAS CATEGORIAS
 # construction_year - Year the waterpoint was constructed
+                 #--- con cero hay 18920 ---1960 al 2013
+
+#extraction_type_class - The kind of extraction the waterpoint uses
+                #---ES CASI IGUAL A TYPE_GROUP MEJOR QUEDARNOS CON TYPE_CLASS
+
 # management - How the waterpoint is managed
+                  #--12 categorias
 # payment_type - What the water costs
+                  #--desconocido: 7725
+
 # quality_group - The quality of the water
+                  #--desconocido 1841
 # quantity - The quantity of water
+                  #--desconocido 775
+
 # source - The source of the water
+                  #--desconocido 62
+
 # waterpoint_type_group - The kind of waterpoint
 
 
@@ -87,8 +109,10 @@ summary(status)
 
 ##Variables anidadas 
 #  region_code con subvillage
-# scheme_management con scheme_name
-# extraction_type_class con extraction_type_group
+#scheme_management con scheme_name 
+####### extraction_type_class con extraction_type_group:
+                        #---NO SON ANIDADAS HAY UNAS QUE SON LA MISMA CATEGORIA
+
 
 
 #### PRIMER MODELO!!!! WOOOOOOOO
