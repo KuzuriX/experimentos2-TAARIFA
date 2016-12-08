fd76bb60a685856b2f10129fe47e15d608fc5de2
KuzuriX <ramonosx@Migues-MacBook-Pro.local>
2016-12-07 21:08:30 -0600

    base lista

--- analisis.R
+++ analisis.R
@@ -6,6 +6,7 @@ setwd('/Users/ramonosx/Documents/GitHub/experimentos2-TAARIFA')
 # Librerias
 
 library('dplyr')
+library('fmsb')
 
 # Carga de los datos
 ## LA VARIABLE A PREDECIR ES status_group
@@ -18,56 +19,60 @@ base<-merge(predictoras,respuesta,by= 'id')
 predictoras<-NULL
 respuesta<-NULL
 
-attach(base)
-
 
 # Analisis preliminar
-
-table(status_group)
-
-## anidamiento de lugares
-
-length(levels(region))
-length(levels(subvillage))
-levels(subvillage)
-
-levels(as.factor(region_code))
-
-length(levels(as.factor(region_code)))
-
-length(levels(as.factor(district_code)))
-
-length(base$region[region_code=99])
-
-apply(table(region_code,region), 1, sum)
-
-dim(table(region_code,region))
-
-table(region_code, district_code)
-
-table(region, district_code)
-
-
-
-table(status_group)
 
 ##VAR respuesta con ceros y unos
 
 base<- base[!(status_group=="functional needs repair"),] 
 base$status = 1*(base$status_group=='functional') 
-attach(base)
 summary(status)
 
 
 ##### limpieza de variables 
 
-basepayment_type
+
+##################   MIGUEL  ########################
+
+#Funder - reduce factor levels
+NUM_LEVELS_FUNDER = 10 #Funder will have this many + 1 levels
+#############################################################################################
+length(levels(funder))
+funderNames <- names(summary(base$funder)[1:NUM_LEVELS_FUNDER])
+funder <- factor(base$funder, levels=c(funderNames, "Other"))
+funder[is.na(funder)] <- "Other"
+base$funder <- funder
+length(levels(funder))
+
+
+#Installer - reduce factor levels
+NUM_LEVELS_INSTALLER = 10 #Installer will have this many + 1 levels
+#############################################################################################
+length(levels(installer))
+
+installerNames <- names(summary(base$installer)[1:NUM_LEVELS_INSTALLER])
+installer <- factor(base$installer, levels=c(installerNames, "Other"))
+installer[is.na(installer)] <- "Other"
+base$installer <- installer
+length(levels(installer))
+
+base<-base[!(base$management=='unknown'),]
+base<-base[!(base$scheme_management==''),]
+base<-base[!(base$source=='unknown'),]
+base<-base[!(base$quantity=='unknown'),]
+base<-base[!(base$payment_type=='unknown'),]
+base<-base[!(base$quality_group=='unknown'),]
+base<-base[!(base$construction_year==0),]
+base$age<-base$construction_year-1960
+
+
 
 
 
 #### PRIMER MODELO!!!! WOOOOOOOO
 set.seed(123)
-base1<- base  %>% sample_n(1000)
+train<- base  %>% sample_n(1000)
+test<- base  %>% sample_n(200)
 
 # 
 # ## 50% of the sample size
@@ -80,143 +85,35 @@ base1<- base  %>% sample_n(1000)
 # train1 <- train[train_ind, ]
 # test1 <- train[-train_ind, ]
 
-mod1<-glm(status~region_code+subvillage+amount_tsh+gps_height+installer+funder
-          +basin+population+scheme_management+scheme_name+construction_year
+mod1<-glm(status~region+amount_tsh+gps_height+installer+funder
+          +basin+population+scheme_management+age
           +extraction_type_class+extraction_type_group+management+payment_type
           +quality_group+quantity+source+waterpoint_type_group,
-          family = 'binomial', data = base1)
+          family = 'binomial', data = train)
 
 summary(mod1)
-drop1mod1<-drop1(mod1, test='LRT')
-
-step1mod1<-step(mod1,direction="backward")
-
-
-##################   MIGUEL  ########################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################   NATY  ########################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################   ANDRES  ########################
+NagelkerkeR2(mod1)
+
+drop1mod1<-drop1(mod1, test='LRT');drop1mod1
+
+step1mod1<-step(mod1,direction="backward"); step1mod1
+
+mod2<-glm(status ~ region + funder + population + age + extraction_type_group + 
+            management + payment_type + quantity + source + waterpoint_type_group, 
+          family = "binomial", data = train)
+
+summary(mod2)
+NagelkerkeR2(mod2)
+
+mod3<-glm(status ~ region + funder + population + age + extraction_type_group + 
+            management + payment_type + quantity + source + waterpoint_type_group, 
+          family = "binomial", data = train)
+drop1(mod3, test='LRT')
+NagelkerkeR2(mod3)
+
+mod4<-glm(status ~ region*population*quantity*waterpoint_type_group 
+          *funder*management * age * extraction_type_group * 
+          payment_type * source , 
+          family = "binomial", data = train)
+drop1(mod4, test='LRT')
+NagelkerkeR2(mod3)
