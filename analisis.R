8f9743b6dccd771582267400016394afb462f6a3
KuzuriX <ramonosx@Migues-MacBook-Pro.local>
2016-12-11 17:01:17 -0600

    colapso de categorias y ods

--- analisis.R
+++ analisis.R
@@ -10,6 +10,7 @@ library('fmsb')
 library('lme4')
 library('lmerTest')
 library(lattice)
+library(piecewiseSEM)
 options(contrasts=c("contr.sum","contr.poly"))
 
 # Carga de los datos
@@ -62,6 +63,12 @@ length(levels(installer))
 
 base$waterpoint_type_group[base$waterpoint_type_group=="dam"]<-"other"
 base$waterpoint_type_group[base$waterpoint_type_group=="cattle trough"]<-"other"
+base$management[base$management=="other - school"]<-"other"
+base$management[base$management=="trust"]<-"other"
+base$management[base$management=="company"]<-"private operator"
+base$management[base$management=="water board"]<- "water authority"
+
+
 
 base<-base[!(base$management=='unknown'),]
 base<-base[!(base$scheme_management==''),]
@@ -81,94 +88,88 @@ set.seed(123)
 train<- base  %>% sample_n(1000)
 test<- base  %>% sample_n(200)
 
+
+
 # 
-# ## 50% of the sample size
-# smp_size <- floor(0.8 * nrow(base))
+# #modelo con todo
 # 
-# ## set the seed to make your partition reproductible
+# mod1<-glmer(status~region+amount_tsh+gps_height+(1|installer)+(1|funder)
+#           +basin+population+scheme_management+age
+#           +extraction_type_class+extraction_type_group+management+payment_type
+#           +quality_group+quantity+source+(1|waterpoint_type_group),
+#           family = 'binomial', data = train)
 # 
-# train_ind <- sample(seq_len(nrow(train)), size = smp_size)
+# #todas las interacciones
 # 
-# train1 <- train[train_ind, ]
-# test1 <- train[-train_ind, ]
-
-
-#modelo con todo
-
-mod1<-glmer(status~region+amount_tsh+gps_height+(1|installer)+(1|funder)
-          +basin+population+scheme_management+age
-          +extraction_type_class+extraction_type_group+management+payment_type
-          +quality_group+quantity+source+(1|waterpoint_type_group),
-          family = 'binomial', data = train)
-
-#todas las interacciones
-
-mod2<-lmer(status~region*amount_tsh*gps_height*(1|installer)*(1|funder)
-            *basin*population*scheme_management*age
-            *extraction_type_class*extraction_type_group*management*payment_type
-            *quality_group*quantity*source*(1|waterpoint_type_group)
-            , data = train)
-
-summary(mod1)
-NagelkerkeR2(mod1)
-
-drop1mod1<-drop1(mod1, test='LRT');drop1mod1
-
-step1mod1<-lmerTest::step(mod2); step1mod1
-
-
-mod3<-glmer(status ~ region + amount_tsh + gps_height + 
-            (1 | funder) + population + age + extraction_type_class + 
-            management + payment_type + quantity + source + 
-              (1 | waterpoint_type_group), 
-          family = "binomial", data = train)
-
-summary(mod3)
-NagelkerkeR2(mod2)
-
-
-mod4<-glmer(status ~ region + amount_tsh + population + 
-              +gps_height
-              +extraction_type_class 
-              +age 
-              +management 
-              +source 
-            +payment_type
-              +quantity
-            + (1 | funder) +
-              (1 | waterpoint_type_group:quantity), 
-            family = "binomial", data = train)
-
-anova(mod3,mod4)
-
-
-###### modelo 
-
-modFinal<-glmer(status ~ region + amount_tsh + population + 
-                  +gps_height
-                +extraction_type_class 
-                +age 
-                +management 
-                +source 
-                +payment_type
-                +quantity
-                + (1 | funder) 
-                + (1 | funder:amount_tsh) 
-                +(1 | funder:population) 
-                + (1 | waterpoint_type_group)
-                 + (1 | waterpoint_type_group:population)
-+ (1 | waterpoint_type_group:gps_height)
-   +  (1 | waterpoint_type_group:extraction_type_class)
-       +  (1 | waterpoint_type_group:age)
-       +  (1 | waterpoint_type_group:payment_type), 
-                family = "binomial", data = train)
-
-summary(modFinal)
-drop1(modFinal, test="Chisq")
+# mod2<-lmer(status~region*amount_tsh*gps_height*(1|installer)*(1|funder)
+#             *basin*population*scheme_management*age
+#             *extraction_type_class*extraction_type_group*management*payment_type
+#             *quality_group*quantity*source*(1|waterpoint_type_group)
+#             , data = train)
+# 
+# summary(mod1)
+# NagelkerkeR2(mod1)
+# 
+# drop1mod1<-drop1(mod1, test='LRT');drop1mod1
+# 
+# step1mod1<-lmerTest::step(mod2); step1mod1
+# 
+# 
+# mod3<-glmer(status ~ region + amount_tsh + gps_height + 
+#             (1 | funder) + population + age + extraction_type_class + 
+#             management + payment_type + quantity + source + 
+#               (1 | waterpoint_type_group), 
+#           family = "binomial", data = train)
+# 
+# summary(mod3)
+# NagelkerkeR2(mod2)
+# 
+# 
+# mod4<-glmer(status ~ region + amount_tsh + population + 
+#               +gps_height
+#               +extraction_type_class 
+#               +age 
+#               +management 
+#               +source 
+#             +payment_type
+#               +quantity
+#             + (1 | funder) +
+#               (1 | waterpoint_type_group:quantity), 
+#             family = "binomial", data = train)
+# 
+# anova(mod3,mod4)
+# 
+# 
+# ###### modelo 
+# 
+# modFinal<-glmer(status ~ region + amount_tsh + population + 
+#                   +gps_height
+#                 +extraction_type_class 
+#                 +age 
+#                 +management 
+#                 +source 
+#                 +payment_type
+#                 +quantity
+#                 + (1 | funder) 
+#                 + (1 | funder:amount_tsh) 
+#                 +(1 | funder:population) 
+#                 + (1 | waterpoint_type_group)
+#                  + (1 | waterpoint_type_group:population)
+# + (1 | waterpoint_type_group:gps_height)
+#    +  (1 | waterpoint_type_group:extraction_type_class)
+#        +  (1 | waterpoint_type_group:age)
+#        +  (1 | waterpoint_type_group:payment_type), 
+#                 family = "binomial", data = train)
+# 
+# summary(modFinal)
+# drop1(modFinal, test="Chisq")
 
 ##interacciones
 drop1(mod4, test='LRT')
 NagelkerkeR2(modFinal)
+
+
+
 
 #########vamos a asumir que no hay interaccion
 
@@ -179,8 +180,9 @@ modFinal2<-glmer(status ~ region + amount_tsh + population +
 
 summary(modFinal2)
 
-#nagelkere 
-NagelkerkeR2(modFinal2)
+#R2 
+
+rsquared(modFinal2)
 
 #referencias
 contrasts(factor(train$status)) # referencia es el 1: funcionan
@@ -197,7 +199,7 @@ mod1<-glmer(status ~amount_tsh + population +
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 
 anova(mod1,modFinal2)
-#Se obtuvo: estadistico=20.664 y p-value=0.1918 --no se rechaza 
+#Se obtuvo: estadistico=26.312 y p-value=0.04979 -- se rechaza 
 
 #2.amount_tsh 
 
@@ -207,7 +209,7 @@ mod2<-glmer(status ~ region + population +
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 
 anova(mod2,modFinal2)
-#Se obtuvo: estadistico=4.91 y p-value=0.0267 --se rechaza , var continua
+#Se obtuvo: estadistico=3.9941 y p-value=0.04566 --se rechaza , var continua
 
 
 
@@ -224,14 +226,14 @@ mod4<-glmer(status ~ region + amount_tsh + population + extraction_type_class +a
                  +management+source+payment_type+quantity+ (1 | funder) 
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 anova(mod4,modFinal2)
-#Se obtuvo: estadistico=0.997 y p-value=0.318 --no se rechaza , var continua
+#Se obtuvo: estadistico=0.3859 y p-value=0.5345 --no se rechaza , var continua
 
 #5 extraction_type_class 
 mod5<-glmer(status ~ region + amount_tsh + population +gps_height+age 
                  +management+source+payment_type+quantity+ (1 | funder) 
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 anova(mod5,modFinal2)
-#Se obtuvo: estadistico=11.249 y p-value=0.04665 -- se rechaza 
+#Se obtuvo: estadistico=13.865 y p-value=0.01649 -- se rechaza 
 
 #6 age
 mod6<-glmer(status ~ region + amount_tsh + population +gps_height+extraction_type_class
@@ -239,20 +241,20 @@ mod6<-glmer(status ~ region + amount_tsh + population +gps_height+extraction_typ
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 
 anova(mod6,modFinal2)
-#Se obtuvo: estadistico=19.318 y p-value=1.106e-05 -- se rechaza , var continua
+#Se obtuvo: estadistico=20.027 y p-value=17.635e-06 -- se rechaza , var continua
 
 #7management
 mod7<-glmer(status ~ region + amount_tsh + population+gps_height+extraction_type_class +age 
                  +source+payment_type+quantity+ (1 | funder)+(1 | waterpoint_type_group), family = "binomial", data = train)
 anova(mod7,modFinal2)
-#Se obtuvo: estadistico=33.303 y p-value=0.00024 -- se rechaza 
+#Se obtuvo: estadistico=8.4876 y p-value=0.2045 -- NO se rechaza 
 
 #8source
 mod8<-glmer(status ~ region + amount_tsh + population +gps_height+extraction_type_class +age 
                  +management+payment_type+quantity+ (1 | funder) 
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 anova(mod8,modFinal2)
-#Se obtuvo: estadistico=17.08 y p-value=0.02929 -- se rechaza 
+#Se obtuvo: estadistico=21.317 y p-value=0.006352 -- se rechaza 
 
 
 #9payment_type
@@ -260,7 +262,7 @@ mod9<-glmer(status ~ region + amount_tsh + population+gps_height+extraction_type
                  +management+source+quantity+ (1 | funder) 
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 anova(mod9,modFinal2)
-#Se obtuvo: estadistico=27.137 y p-value=5.363e-05 -- se rechaza 
+#Se obtuvo: estadistico=22.477 y p-value=0.0004248 -- se rechaza 
 
 
 #10quantity
@@ -268,7 +270,7 @@ mod10<-glmer(status ~ region + amount_tsh + population+gps_height+extraction_typ
                  +management+source+payment_type+(1 | funder) 
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 anova(mod10,modFinal2)
-#Se obtuvo: estadistico=147.02 y p-value=2.2e-16 -- se rechaza 
+#Se obtuvo: estadistico=151.02 y p-value=2.2e-16 -- se rechaza 
 
 
 ####aleatorias
@@ -281,7 +283,7 @@ mod11<-glmer(status ~ region + amount_tsh + population +
                  +(1 | waterpoint_type_group), family = "binomial", data = train)
 
 anova(mod11,modFinal2)
-#Se obtuvo: estadistico=3.1021 y p-value=0.07819 -- no se rechaza 
+#Se obtuvo: estadistico=5.0903 y p-value=0.02406 -- no se rechaza 
 
 
 #12(1 | waterpoint_type_group)
@@ -291,7 +293,7 @@ mod12<-glmer(status ~ region + amount_tsh + population +
                  +management+source+payment_type+quantity+ (1 | funder), family = "binomial", data = train)
 
 anova(mod12,modFinal2)
-#Se obtuvo: estadistico=10.602 y p-value=0.00113 -- se rechaza 
+#Se obtuvo: estadistico=7.776 y p-value=0.005294 -- se rechaza 
 
 ###aleatorias hacer graficos de prediccion 
 #modelo sin restringir
@@ -300,7 +302,7 @@ modFinal2_sin<-glmer(status ~ region + amount_tsh + population +
                    +gps_height+extraction_type_class +age 
                  +management+source+payment_type+quantity+ (1 | funder) 
                  +(1 | waterpoint_type_group), family = "binomial", data = train,REML=F)
-summary(modFinal2)
+summary(modFinal2_sin)
 
 #no sirve
 prof=profile(modFinal2_sin)
@@ -314,13 +316,13 @@ confint(modFinal2,level=0.95,trace=FALSE)
 
 
 #OR
-
+coefs<-data.table(summary(modFinal2_sin)$coef[,1:2])
 
 #continuas
 #amount
 
-beta_a=0.000234
-sd_beta=0.00012
+beta_a=coefs['amount_tsh',1]
+sd_beta=coefs['amount_tsh',2]
 
 OR_amount=exp(100*beta_a)  
 OR_amount  
@@ -333,10 +335,10 @@ ic_OR_amount
 
 #age
 
-beta_age=0.0373
-sd_beta_age=0.0085
+beta_age=coefs['age',1]
+sd_beta_age=coefs['age',2]
 
-OR_age=exp(5*beta_age)  #5 años
+OR_age=exp(5*beta_age)  #5 a?os
 OR_age 
 
 z_bonf=pnorm(1-(0.05/(2*51)))
@@ -347,8 +349,8 @@ ic_OR_age
 #categoricas
 
 #########extraction 
-betas_extrac=c(0.116,1.3,-0.375,-0.205,-0.103)
-sd_beta_extrac=c(0.481,0.502,0.577,0.453,0.825)
+betas_extrac=coefs[paste0("extraction_type_class",c(1,2,3,4,5)),1]
+sd_beta_extrac=coefs[paste0("extraction_type_class",c(1,2,3,4,5)),2]
 
 
 #para hacer ic
@@ -381,11 +383,11 @@ colnames(mat_or_extract)=c("OR","Lim inf","Lim sup")
 mat_or_extract
 
 
-####managment
+####region
 #############REVISAR LOS IC DE OR SE INDEFINEN
 
-betas=c(-2.384,1.074,-17.9,0.839,1.194,16.94,-0.128,-0.465,0.309,0.789)
-sd_beta=c(802.1,802.1,4174,802.1,802.1,6896,802.1,802.1,802.1,802.1)
+betas=coefs[paste0("region",c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)),1]
+sd_beta=coefs[paste0("region",c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)),2]
 
 
 #para hacer ic
@@ -414,6 +416,7 @@ for(i in 1:length(comb[1,])){
   ic_OR[i,] =exp(ic_b)
 }
 mat_or=cbind(OR,ic_OR)
+##hay que poberle los nombres a las comparaciones
 rownames(mat_or)=c("1-2","1-3","1-4","1-5","1-6","1-7","1-8","1-9","1-10","2-3"
                            ,"2-4","2-5","2-6","2-7","2-8","2-9","2-10","3-4","3-5","3-6"
                            ,"3-7","3-8","3-9","3-10","4-5","4-6","4-7","4-8","4-9","4-10"
@@ -426,10 +429,11 @@ mat_or
 
 
 
+
 ####SOURCE
 
-betas=c(0.0088,-0.531,-2.316,0.13,-0.258,1.64,1.006,-0.38)
-sd_beta=c(0.559,0.736,1.168,0.354,1.162,0.81,0.3677,0.424)
+betas=coefs[paste0("source",c(1,2,3,4,5,6,7,8)),1]
+sd_beta=coefs[paste0("source",c(1,2,3,4,5,6,7,8)),2]
 
 #para hacer ic
 dif_betas=diff(combn(betas,2))
@@ -461,8 +465,8 @@ mat_or
 
 ####PAYMENT_TYPE
 
-betas=c(0.248,0.779,-0.447,0.2397,-1.461)
-sd_beta=c(0.3919,0.2841,0.2464,0.3735,0.7677)
+betas=coefs[paste0("source",c(1,2,3,4,5)),1]
+sd_beta=coefs[paste0("source",c(1,2,3,4,5)),2]
 
 #para hacer ic
 dif_betas=diff(combn(betas,2))
@@ -495,8 +499,8 @@ mat_or
 
 ####quantity  ######da raro
 
-betas=c(-14.96,5.15,4.66)
-sd_beta=c(757.5,252.5,252.5)
+betas=coefs[paste0("quantity",c(1,2,3)),1]
+sd_beta=coefs[paste0("quantity",c(1,2,3)),2]
 
 #para hacer ic
 dif_betas=diff(combn(betas,2))
